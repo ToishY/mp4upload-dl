87b599e8eb48dc722a7466cccaf9286f14744c21
ToshY <31921460+ToshY@users.noreply.github.com>
2021-01-02 15:54:32 +0100

    Update MP4DL.php

--- MP4DL.php
+++ MP4DL.php
@@ -1,7 +1,8 @@
 <?php
 function MP4DL($embedId){
-	preg_match('/(?P<server>w{3}\d{1,2}).*(?P<string>[a-zA-Z0-9]{56}).(?P<port>\d{3})/', file_get_contents("https://www.mp4upload.com/embed-".$embedId.".html"), $result);
-  	return "https://".$result["server"].".mp4upload.com:".$result["port"]."/d/".$result["string"]."/video.mp4";
+	preg_match_all('/<h2>Download File (?P<file>.*?)<\/h2>/', file_get_contents("https://www.mp4upload.com/".$embedId), $f);
+	preg_match_all('/(?P<server>www\d{1,2}|s\d{1})\|.*\|poster\|([a-z0-9]+)\|([nivideo|video]+)\|(?P<string>[a-z0-9]{56})\|(?P<port>\d{1,3})/', file_get_contents("https://www.mp4upload.com/embed-".$embedId.".html"), $m);
+	return sprintf('https://%s.mp4upload.com:%s/d/%s/%s', $m['server'][0], $m['port'][0], $m['string'][0], $f['file'][0]);
 }
 #how to use
 echo MP4DL("f00lb4rz");
